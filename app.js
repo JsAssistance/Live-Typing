7e2857b285da83a0ce4101336f286f22e4188bec
Gauthier SAMBA SEALE <gssbartender@gmail.com>
2022-12-09 11:40:05 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const input = document.querySelector('input');
+
+const result = document.querySelector('.result');
+
+input.addEventListener('keyup', () => {
+    result.innerHTML = input.value;
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('input');
+
+// const result = document.querySelector('.result');
+
+
+// input.addEventListener('keyup', () => {
+//     result.innerHTML = input.value;
+// });
+
+
